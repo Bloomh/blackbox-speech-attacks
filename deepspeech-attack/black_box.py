26c1b4fba5dfd84cdc97eaf071c5ef55fcaccb1e
Henry Bloom <hmbloom@uchicago.edu>
2025-05-27 17:26:02 -0500

    testing with more models in ensemble

--- deepspeech-attack/black_box.py
+++ deepspeech-attack/black_box.py
@@ -77,10 +77,8 @@ if __name__ == "__main__":
     # ENSEMBLE INIT
     
     # if the attack mode has been specified as ENSEMBLE, manually modify ensemble here.
-    # ensemble_versions = ["v1", "v2", "v2", "v1"] # we might have more of each if we get pretrained models on diff datasets
-    # ensemble_training_sets = ["librispeech", "librispeech", "ted", "an4"]
-    ensemble_versions = ["v2"]
-    ensemble_training_sets = ["librispeech"]
+    ensemble_versions = ["v1", "v2", "v2", "v1"] # we might have more of each if we get pretrained models on diff datasets
+    ensemble_training_sets = ["librispeech", "librispeech", "ted", "an4"]
     
 
     # Run attack
